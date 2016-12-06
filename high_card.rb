8d18cbe5c28797ea0fc34b8446eecd518bd09752
“neilburd” <neilburd@gmail.com>
2016-12-06 16:55:03 -0500

    made the deck of cards

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,9 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = ranks.each do |ranks|
+   suits.each do |suits|
+    puts "#{ranks} #{suits}"
+   end
+ end  
