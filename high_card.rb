fae70a3caed867ca9d6734b8592e144c31c0094c
Erik Shepard-Lewis <adelina198527@gmail.com>
2016-12-06 18:12:07 -0500

    Update high_card.rb

--- high_card.rb
+++ high_card.rb
@@ -1,11 +1,16 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck=[]
+deck2=[]
 
 players = []
 
-deck = ranks.each do |ranks|
+ ranks.each do |ranks|
    suits.each do |suits|
     puts "#{ranks} #{suits}"
+   
+ deck2=[ranks,suits]
+ deck.push(deck2)  
    end
  end  
