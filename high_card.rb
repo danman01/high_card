41f941cad6661db7fc2b9b1e65ad2d6d71b61f8f
jtellis <jessetellis@gmail.com>
2016-12-06 16:43:18 -0500

    Create shuffled deck and player input

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,23 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = []
+
+ranks.each do |rank|
+  suits.each do |suit|
+    deck.push([rank, suit])
+  end
+end
+
+deck.shuffle!
+
 players = []
+user_input = ''
+
+while user_input != 'play'
+  puts "#{players.length} players so far. Enter a player name or enter 'play'"
+  user_input = gets.chomp
+  if user_input.length > 0 && user_input != 'play'
+    players.push({name: user_input})
+  end
+end
