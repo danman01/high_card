81639b497fc1bf9cf21d6a5d13582815606fe7d8
yourUsername <your_email@example.com>
2016-12-07 09:01:52 -0500

    Attempted High Card HW with Julie

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,20 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
 players = []
+
+deck = []
+suits.each do |suit|
+  ranks.each do |rank|
+    deck.push("#{rank}, #{suit}")
+  end
+end
+deck.shuffle!
+
+
+while true
+  puts "#{players.length} players so far. Enter a player name, or type 'play'."
+  name = gets.chomp
+  break if name == "play"
+  players.push('name: ' + name)
+end
