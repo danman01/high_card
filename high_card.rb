b43a2ba6ee7a587baa69d07b66880c7e924a7584
murphypicard <ayaghsizian@gmail.com>
2016-12-07 11:16:56 -0500

    Ara and Mason

--- high_card.rb
+++ high_card.rb
@@ -1,13 +1,18 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 players = []
 deck = []
-ranks.each do |rank|
-  suits.each do |suit|
-    deck.push("[#{rank}, #{suit}]")
+
+
+v = 0
+ranks.each do |r|
+  suits.each do |s|
+    v += 1
+    deck.push({rank: r, suit: s, value: v})
   end
 end
+
 deck.shuffle!
 
 while true
@@ -16,3 +21,34 @@ while true
   break if name == "play"
   players.push(name)
 end
+
+cards = players.map do
+  deck.pop
+end
+
+values = cards.map do |card|
+  card[:value]
+end
+
+# values = players.map do |name|
+#   name[:players]
+# end
+
+high_score = values.max
+
+x = 0
+players.each do
+  puts "#{players[x]} chose #{cards[x]}"
+  x += 1
+end
+
+puts "this is the high_score"
+puts high_score
+
+winners = []
+
+values.each_with_index do |value, index|
+  winners.push(players[index]) if value == high_score
+end
+
+puts "#{winners[0]} wins"
